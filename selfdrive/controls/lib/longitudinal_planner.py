657a4c78d5c0597e7c0627352903dd014bf0067e
SusanYu25 <yuhong299@gmail.com>
2024-10-27 13:33:20 +0800

    Update longitudinal_planner.py

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -25,13 +25,13 @@ from openpilot.selfdrive.controls.lib.events import Events
 from openpilot.common.swaglog import cloudlog
 
 LON_MPC_STEP = 0.2  # first step is 0.2s
-A_CRUISE_MIN = -1.2
+A_CRUISE_MIN = -2.0
 A_CRUISE_MAX_VALS = [3.0, 1.7, 1.3, 0.8, 0.6, 0.44, 0.32, 0.22, 0.16, 0.0078]
 A_CRUISE_MAX_BP =   [0.,  3,   6.,  8.,  11., 15.,  20.,  25.,  30.,  55.]
 CONTROL_N_T_IDX = ModelConstants.T_IDXS[:CONTROL_N]
 
 # Lookup table for turns
-_A_TOTAL_MAX_V = [1.7, 3.2]
+_A_TOTAL_MAX_V = [2.0, 3.2]
 _A_TOTAL_MAX_BP = [20., 40.]
 
 
